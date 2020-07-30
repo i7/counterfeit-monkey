i7/counterfeit-monkey

commit f2e2c4736cf92a5fa54723227b524aefaa3280a1

Fix exhausted Brock-memory
When we don't actually enter the memory, we don't need the
message "Then we’re back in the present."

Counterfeit Monkey.materials/Extensions/Counterfeit Monkey/Memories.i7x
@@ -176,6 +176,9 @@ Carry out examining a bed:
 Carry out examining a dorm bed:
 	trigger Brock-bed.
 
+Check remembering Brock-bed:
+	if the uses of the noun is greater than 5:
+		say "I get the idea. You don't need to show me more." instead.
 
 Carry out remembering Brock-bed:
 	if the uses of the noun is:

commit a3b3a8940989522181c37d527a721726e7d947bb

Fix off-by-one error
This was introduced in f2e2c47 to remove the redundant "Then we’re back in the present."
But at the time of that check, the "uses" property of the memory has not been incremented
yet, so the redundant message was still printed once.

Counterfeit Monkey.materials/Extensions/Counterfeit Monkey/Memories.i7x
@@ -177,7 +177,7 @@ Carry out examining a dorm bed:
 	trigger Brock-bed.
 
 Check remembering Brock-bed:
-	if the uses of the noun is greater than 5:
+	if the uses of the noun is greater than 4:
 		say "I get the idea. You don't need to show me more." instead.
 
 Carry out remembering Brock-bed: