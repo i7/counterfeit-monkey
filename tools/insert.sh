072c827b2c48fafa5649d5a05238304611b064d9
angstsmurf <ignalina@me.com>
2023-02-02 21:48:07 +0100

    Update insert.sh script for macOS 12
    Just specify Python 2.

--- tools/insert.sh
+++ tools/insert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "y" | python blorbtool.py "../Counterfeit Monkey.materials/Release/Counterfeit Monkey.gblorb" import Data 9998 IFZS "Counterfeit Monkey-startup-data.glkdata"
+echo "y" | python2 blorbtool.py "../Counterfeit Monkey.materials/Release/Counterfeit Monkey.gblorb" import Data 9998 IFZS "Counterfeit Monkey-startup-data.glkdata"
