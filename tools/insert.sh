31111c33b5949e590cec14e4241179e20464f88a
angstsmurf <ignalina@me.com>
2023-06-24 22:36:28 +0200

    Make insert.sh more useful
    This effectively reverts c431c2e. After 69d5016, it should finally work as
    intended.

--- tools/insert.sh
+++ tools/insert.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+path_to_interpreter=./git
+testfile="command scripts/test_minimal.txt"
+
+rm monkeyac.glkdata
+rm testing.glkdata
+rm "Counterfeit Monkey-startup-data.glkdata"
+
+$path_to_interpreter "../Counterfeit Monkey.materials/Release/Counterfeit Monkey.gblorb" <"$testfile"  > transcript.txt
+
 echo "y" | python2 blorbtool.py "../Counterfeit Monkey.materials/Release/Counterfeit Monkey.gblorb" import Data 9998 IFZS "Counterfeit Monkey-startup-data.glkdata"
