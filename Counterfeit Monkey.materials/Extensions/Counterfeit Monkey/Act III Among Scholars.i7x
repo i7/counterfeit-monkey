i7/counterfeit-monkey

commit 9c5cbe95a6df3764a2e7c956a0cc7c8694b3e9c1

Highlight the bathroom window
Yes, in one case we highlight "out" but not "the window". I think that makes
more sense than highlighting "out the window".

Counterfeit Monkey.materials/Extensions/Counterfeit Monkey/Act III Among Scholars.i7x
@@ -458,10 +458,10 @@ Sanity-check locking keylessly the bathroom window:
 
 
 Report opening the bathroom window for the first time:
-	say "The window slides open with a hideous protesting shriek." instead.
+	say "The [if boldening is true][bold type][end if]window[roman type] slides open with a hideous protesting shriek." instead.
 
 Report closing the bathroom window:
-	say "[We] slide the window shut[one of]. I feel obscurely better now[or][stopping]." instead.
+	say "[We] slide the [if boldening is true][bold type][end if]window[roman type] shut[one of]. I feel obscurely better now[or][stopping]." instead.
 
 Before: if the noun is the bathroom window or the second noun is the bathroom window,
 	reveal bathroom window.
@@ -509,7 +509,7 @@ In-direction of Apartment Bathroom is south. [My apartment]
 Out-direction of Apartment Bathroom is east. [Palm Square]
 
 Rule for listing exits when the location is Apartment Bathroom:
-	say "[We] could climb back out the window, or [we] could go south into the rest of my apartment."
+	say "[We] could climb back [if boldening is true][bold type][end if]out[roman type] the window, or [we] could go [south] into the rest of my apartment."
 
 A nightstand is a supporter in the apartment bathroom. The initial appearance is "An antique [nightstand] of my mother's, which does not actually fit anywhere near my bed, is jammed into one corner of the bathroom."
 

commit 009b3f4d4e2ea4540bcd9765856f7ddf256d9a81

Fix bad message caused by 9c5cbe9
Due to how Exit Lister works, we have to do it this way instead.
(It would say "we could go south to my apartment into the rest of my apartment.")

Counterfeit Monkey.materials/Extensions/Counterfeit Monkey/Act III Among Scholars.i7x
@@ -509,7 +509,7 @@ In-direction of Apartment Bathroom is south. [My apartment]
 Out-direction of Apartment Bathroom is east. [Palm Square]
 
 Rule for listing exits when the location is Apartment Bathroom:
-	say "[We] could climb back [if boldening is true][bold type][end if]out[roman type] the window, or [we] could go [south] into the rest of my apartment."
+	say "[We] could climb back [if boldening is true][bold type][end if]out[roman type] the window, or [we] could go [if boldening is true][bold type][end if]south[roman type] into the rest of my apartment."
 
 A nightstand is a supporter in the apartment bathroom. The initial appearance is "An antique [nightstand] of my mother's, which does not actually fit anywhere near my bed, is jammed into one corner of the bathroom."
 